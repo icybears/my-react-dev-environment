85ca1e2ae98f252710d6de0721a0612731ba44ec
Saber I <isaber23@outlook.com>
2017-09-10 16:55:18 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './app/index.js',
@@ -23,7 +24,21 @@ module.exports = {
      })
  ]
 };
+// the following is only relevant for the build process
+if(process.env.NODE_ENV === 'production'){
+  config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env' : {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+        new webpack.optimize.UglifyJsPlugin()
+     )
+}
 
+module.exports = config;
+
+//note:
 //the HtmlWebpackPlugin we installed and added as a plugin will take our index.html,
 //and create a new index.html inside the dist folder based on that original one,
 //and automatically add a script tag to it referencing our index_bundle.js.
